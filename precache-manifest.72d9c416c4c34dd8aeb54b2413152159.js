2d2f37fc3e64082be9766e56fbd4df7703b88275
nikostzam <nikosczt@gmail.com>
2020-03-04 20:31:48 +0200

    Updates

---
+++ precache-manifest.72d9c416c4c34dd8aeb54b2413152159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca9f81c2699cb928a2ab86f910620bc",
+    "url": "/todo-v2/index.html"
+  },
+  {
+    "revision": "a750130fe411e1085d6c",
+    "url": "/todo-v2/static/css/main.a4a2c84c.chunk.css"
+  },
+  {
+    "revision": "20d3f6b8cce112744676",
+    "url": "/todo-v2/static/js/2.794e17e7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-v2/static/js/2.794e17e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a750130fe411e1085d6c",
+    "url": "/todo-v2/static/js/main.d40b7f32.chunk.js"
+  },
+  {
+    "revision": "04ab4ec586c13f7b73c1",
+    "url": "/todo-v2/static/js/runtime-main.1a220853.js"
+  }
+]);
